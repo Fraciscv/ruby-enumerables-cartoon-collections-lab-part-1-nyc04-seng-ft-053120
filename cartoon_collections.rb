7908010646f16d838971adfa399b76d626c143fc
Francisco Andaur <francisco.andaur@icloud.com>
2020-07-01 01:13:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,18 @@
+require 'pry'
+
 def greet_characters(array)
-  array.each do | element|
-    puts "Hello #{element}!"
+#  prints out a custom greeting for each name in the provided array
+  array.each do |element|
+#  Hello Dopey!
+    puts"Hello #{element}!"
   end
 end
 
-def list_dwarves(array)
+def list_dwarves(array)  
+# prints out the 7 dwarves in a numbered list
   array.each_with_index do |element, index|
+# /1.*Dopey/
     puts "#{index + 1}. #{element}"
   end
+  
 end
